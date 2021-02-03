0f677a05676c0fdcdf60042afd5c39d6b781505b
Naval63 <b.smalcerz@gmail.com>
2021-02-03 13:21:30 +0100

    add li link and button

--- vege/src/components/Navbar.js
+++ vege/src/components/Navbar.js
@@ -21,8 +21,26 @@ const  Navbar = () => {
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className='nav-item'>
                             <Link to='/' className='nav-links'
-                                onClick={closeMobileMenu}>
+                            onClick={closeMobileMenu}>
                                 Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/service' className='nav-links'
+                            onClick={closeMobileMenu}>
+                                Service
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links'
+                            onClick={closeMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className='nav-links-mobile'
+                            onClick={closeMobileMenu}>
+                                Sign Up
                             </Link>
                         </li>
                     </ul>    
