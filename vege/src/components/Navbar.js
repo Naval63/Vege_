2af58ed417690b626aa2397c08ad3a0ad886765a
Naval63 <b.smalcerz@gmail.com>
2021-02-04 12:33:14 +0100

    add HeroSection

--- vege/src/components/Navbar.js
+++ vege/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
@@ -15,6 +15,10 @@ const  Navbar = () => {
         }
     };
 
+    useEffect(() => {
+        showButton(); 
+    }, []);
+
     window.addEventListener('resize', showButton);
 
     const handleClick = () => setClick(!click);
@@ -23,23 +27,26 @@ const  Navbar = () => {
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo'>
+                    <Link to='/' 
+                        className='navbar-logo'
+                        onClick={closeMobileMenu}
+                    >
                         TRVL <i className='fab fa-typo3' />
                     </Link>
                     <div className='menu-icon'
                         onClick={handleClick}
-                        >
+                    >
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className='nav-item'>
                             <Link to='/' className='nav-links'
-                            onClick={closeMobileMenu}>
+                                onClick={closeMobileMenu}>
                                 Home
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/service' className='nav-links'
+                            <Link to='/service'                 className='nav-links'
                             onClick={closeMobileMenu}>
                                 Service
                             </Link>
